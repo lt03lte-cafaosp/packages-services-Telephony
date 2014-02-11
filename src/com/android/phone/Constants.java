055668b18d69daa7197ee2e8e041925f8763ec2c
Bao Enhui <benhui@codeaurora.org>
2014-02-11 17:20:47 +0800

    Add new Network mode menu list
    Only "4G/3G/2G mode" and "3G/2G mode" should be shown for CMCC
    requirment.
    
    Change-Id: I71444af90e7a110a486b0d492765b486634122b4

--- src/com/android/phone/Constants.java
+++ src/com/android/phone/Constants.java
@@ -160,4 +160,10 @@ public class Constants {
     public static final String SETTINGS_VIBRATE_WHEN_ACCEPTED = "vibrate_on_accepted";
 
     public static final String SETTINGS_IP_PREFIX = "ip_call_prefix_sub";
+    // network mode
+    public static final int NETWORK_MODE_DEFAULT = 0;
+    public static final int NETWORK_MODE_HIDE = 1;
+    public static final int NETWORK_MODE_CMCC = 2;
+    public static final int NETWORK_MODE_TDCDMA = 3;
+    public static final int NETWORK_MODE_LTE = 4;
 }
