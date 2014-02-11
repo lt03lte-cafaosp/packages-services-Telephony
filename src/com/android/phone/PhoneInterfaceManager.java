8e3389d1c47bae9a8d7f1e7de1c998023e4d6624
jiadongz <jiadongz@codeaurora.org>
2014-02-11 21:02:48 +0800

    CSVT: update csvt in Telephony Service App
    Change-Id: If9f2c36c41d73741b4af832b7091a92115b470ba

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -307,6 +307,12 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
             // Never allow the InCallScreen to appear on data-only devices.
             return false;
         }
+        if(PhoneGlobals.getInstance().isCsvtActive()) {
+            Log.d(LOG_TAG, "showCallScreenInternal: csvt is active");
+            Intent mIntent = new Intent("restore_video_call");
+            mApp.sendBroadcast(mIntent);
+            return false;
+        }
         if (isIdle()) {
             return false;
         }
@@ -394,6 +400,8 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
         // For now, protect this call with the MODIFY_PHONE_STATE permission.)
         enforceModifyPermission();
         sendRequestAsync(CMD_SILENCE_RINGER);
+        // If the Csvt ringer is playing, silence it.
+        silenceCsvtRinger();
     }
 
     /**
@@ -408,6 +416,11 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
             if (DBG) log("silenceRingerInternal: silencing...");
             mApp.notifier.silenceRinger();
         }
+    }
+
+    private void silenceCsvtRinger() {
+        Intent intent = new Intent("com.borqs.videocall.action.silencering");
+        mApp.sendBroadcast(intent);
     }
 
     public boolean isOffhook() {
