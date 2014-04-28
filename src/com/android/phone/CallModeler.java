e33bcd7a6470afb7fb90c41632c511b1e8896c8c
Sandeep Kunta <skunta@codeaurora.org>
2014-04-28 14:45:52 +0530

    MSIM:Add null check to avoid phone app crash
    When both subscriptions have incoming calls in alerting state, and
    both of them disconnected at remote end at same time, Call can be null
    in onDisconnect() due to timing issue. Add null check to avoid phone app
    crash.
    
    Change-Id: I5c5448765526cc9b6e856476fa05fe9761c2be0e
    CRs-Fixed: 652172

--- src/com/android/phone/CallModeler.java
+++ src/com/android/phone/CallModeler.java
@@ -385,7 +385,7 @@ public class CallModeler extends Handler {
             mCallMap.remove(conn);
         }
 
-        if (MSimTelephonyManager.getDefault().isMultiSimEnabled()) {
+        if (MSimTelephonyManager.getDefault().isMultiSimEnabled() && (call != null)) {
             mCallManager.clearDisconnected(call.getSubscription());
         } else {
             mCallManager.clearDisconnected();
