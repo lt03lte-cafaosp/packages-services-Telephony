ee6202778230c1a3091518b928bc0895a074fb7a
Venkatraman Nerellapalli <venkataraman.nerellapalli@codeaurora.org>
2015-03-12 11:42:19 +0530

    Telephony: Add new network mode to support 4G
    Add new network mode,NT_MODE_LTE_CDMA_EVDO_GSM in CT and
    default modes
    
    Change-Id: Ia05177f4396ba2511bc60a3947f55d70fb520f0f
    CRs-Fixed: 796166

--- src/com/android/phone/MobileNetworkSettings.java
+++ src/com/android/phone/MobileNetworkSettings.java
@@ -540,6 +540,7 @@ public class MobileNetworkSettings extends PreferenceActivity
                     case Phone.NT_MODE_TD_SCDMA_GSM_WCDMA_LTE:
                     case Phone.NT_MODE_TD_SCDMA_CDMA_EVDO_GSM_WCDMA:
                     case Phone.NT_MODE_TD_SCDMA_LTE_CDMA_EVDO_GSM_WCDMA:
+                    case Phone.NT_MODE_LTE_CDMA_EVDO_GSM:
                         // This is one of the modes we recognize
                         modemNetworkMode = buttonNetworkMode;
                         break;
@@ -575,6 +576,7 @@ public class MobileNetworkSettings extends PreferenceActivity
                     case Phone.NT_MODE_CDMA:
                     case Phone.NT_MODE_CDMA_NO_EVDO:
                     case Phone.NT_MODE_LTE_CDMA_AND_EVDO:
+                    case Phone.NT_MODE_LTE_CDMA_EVDO_GSM:
                         // This is one of the modes we recognize
                         modemNetworkMode = buttonNetworkMode;
                         break;
@@ -732,7 +734,8 @@ public class MobileNetworkSettings extends PreferenceActivity
                         modemNetworkMode == Phone.NT_MODE_TD_SCDMA_WCDMA_LTE ||
                         modemNetworkMode == Phone.NT_MODE_TD_SCDMA_GSM_WCDMA_LTE ||
                         modemNetworkMode == Phone.NT_MODE_TD_SCDMA_CDMA_EVDO_GSM_WCDMA ||
-                        modemNetworkMode == Phone.NT_MODE_TD_SCDMA_LTE_CDMA_EVDO_GSM_WCDMA) {
+                        modemNetworkMode == Phone.NT_MODE_TD_SCDMA_LTE_CDMA_EVDO_GSM_WCDMA ||
+                        modemNetworkMode == Phone.NT_MODE_LTE_CDMA_EVDO_GSM) {
                     if (DBG) {
                         log("handleGetPreferredNetworkTypeResponse: if 1: modemNetworkMode = " +
                                 modemNetworkMode);
@@ -922,6 +925,10 @@ public class MobileNetworkSettings extends PreferenceActivity
                 mButtonPreferredNetworkMode.setSummary(
                         R.string.preferred_network_mode_td_scdma_lte_cdma_evdo_gsm_wcdma_summary);
                 break;
+            case Phone.NT_MODE_LTE_CDMA_EVDO_GSM:
+                mButtonPreferredNetworkMode.setSummary(
+                        R.string.preferred_network_mode_lte_cdma_evdo_gsm_summary);
+                break;
             default:
                 mButtonPreferredNetworkMode.setSummary(
                         R.string.preferred_network_mode_global_summary);
@@ -988,6 +995,11 @@ public class MobileNetworkSettings extends PreferenceActivity
             case Phone.NT_MODE_LTE_CDMA_EVDO_GSM_WCDMA:
                 mButtonEnabledNetworks.setValue(
                         Integer.toString(Phone.NT_MODE_LTE_CDMA_EVDO_GSM_WCDMA));
+                mButtonEnabledNetworks.setSummary(R.string.network_global);
+                break;
+            case Phone.NT_MODE_LTE_CDMA_EVDO_GSM:
+                mButtonEnabledNetworks.setValue(
+                        Integer.toString(Phone.NT_MODE_LTE_CDMA_EVDO_GSM));
                 mButtonEnabledNetworks.setSummary(R.string.network_global);
                 break;
             default:
