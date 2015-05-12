8aff36ac87e4d203432b39ad1d856da47fa5e67d
Susheel nyamala <snyamala@codeaurora.org>
2015-05-12 18:55:09 +0530

    Revert "Telephony: fix getIccOperatorNumeric"
    This reverts commit 5707543863bb7d432fee9a8997cec87df75db00e
    which is causing apn display issues.
    
    Change-Id: Idfe5f87ea43dc217b808063d16822656a7a097a4
    CRs-Fixed: 836551

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -1684,9 +1684,26 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
      */
     public String getIccOperatorNumeric(int subId) {
         String iccOperatorNumeric = null;
-        IccRecords iccRecords = getPhone(subId).getIccCard().getIccRecords();
-        if (iccRecords != null) {
-            iccOperatorNumeric = iccRecords.getOperatorNumeric();
+        int netType = getPhone(subId).getServiceState().getRilDataRadioTechnology();
+        int family = UiccController.getFamilyFromRadioTechnology(netType);
+        if (UiccController.APP_FAM_UNKNOWN == family) {
+            int phoneType = getActivePhoneTypeForSubscriber(subId);
+            switch (phoneType) {
+                case TelephonyManager.PHONE_TYPE_GSM:
+                    family = UiccController.APP_FAM_3GPP;
+                    break;
+                case TelephonyManager.PHONE_TYPE_CDMA:
+                    family = UiccController.APP_FAM_3GPP2;
+                    break;
+            }
+        }
+
+        if (UiccController.APP_FAM_UNKNOWN != family) {
+            int slotId = mSubscriptionController.getPhoneId(subId);
+            IccRecords iccRecords = UiccController.getInstance().getIccRecords(slotId, family);
+            if (iccRecords != null) {
+                iccOperatorNumeric = iccRecords.getOperatorNumeric();
+            }
         }
         return iccOperatorNumeric;
     }
