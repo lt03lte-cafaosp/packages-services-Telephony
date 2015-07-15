5ebedb998f343dd8ecef8ba106585b4bc817a450
Qiang Chen <cqiang@codeaurora.org>
2015-07-15 17:19:37 +0800

    Telephony: Do not set network mode if all card status is absent
    Do not set network mode to G+G if all card status is absent.
    
    Change-Id: I8673a8179536cc7ad650ae1f424e46717a604627
    CRs-fixed: 866158

--- src/com/android/phone/PrimarySubSelectionController.java
+++ src/com/android/phone/PrimarySubSelectionController.java
@@ -403,6 +403,7 @@ public class PrimarySubSelectionController extends Handler implements OnClickLis
         int slot = -1;
         if (!isPrimarySetable()) {
             logd("primary is not setable in any sub!");
+            return;
         } else {
             int prefPrimarySlot = getPrefPrimarySlot();
             int primarySlot = getPrimarySlot();
