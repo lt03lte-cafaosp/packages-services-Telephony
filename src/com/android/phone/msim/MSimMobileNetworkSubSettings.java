bbb2810b5f387d35d1905bf8261b496c1e32fd2d
Rakesh Pallerla <rakesh@codeaurora.org>
2015-12-29 17:18:21 +0530

    Additional changes for RJIL Priamry card feature.
    1. Change the default title from Dual Sim Settings to
       Sim Card Recognition for primary card setting dialog.
    2. Add mcc,mnc to config changes so that activity is
       not re-created on config changed.
    3. Do not run primary card logic if same cards are present
       across reboots or deactivation/activation.
    4. If user changes nwMode from UI, store it as userPrefPrimarySub in DB.
    
    Change-Id: Ia3e2445357d08a33da2de9da4ce35d5ca38228e6
    CRs-Fixed: 956491, 956494, 956015, 956499

--- src/com/android/phone/msim/MSimMobileNetworkSubSettings.java
+++ src/com/android/phone/msim/MSimMobileNetworkSubSettings.java
@@ -637,6 +637,10 @@ public class MSimMobileNetworkSubSettings extends PreferenceActivity
                     mPhone.getPhoneId(), nwMode);
     }
 
+    public boolean isDetect4gCardEnabled() {
+        return SystemProperties.getBoolean("persist.radio.detect4gcard", false);
+    }
+
     private class MyHandler extends Handler {
 
         static final int MESSAGE_GET_PREFERRED_NETWORK_TYPE = 0;
@@ -676,6 +680,21 @@ public class MSimMobileNetworkSubSettings extends PreferenceActivity
             }
             updateButtonPreferredLte();
             updateButtonEnable4g();
+            updateUserPrefPrimarySubIdInDB();
+        }
+
+        private void updateUserPrefPrimarySubIdInDB() {
+            if (isDetect4gCardEnabled()) {
+                int nwMode = getPreferredNetworkMode();
+                if (nwMode == Phone.NT_MODE_LTE_GSM_WCDMA || nwMode == Phone.NT_MODE_GSM_UMTS) {
+                    android.provider.Settings.Global.putInt(
+                            mPhone.getContext().getContentResolver(),
+                            PrimarySubSelectionController.SETTING_USER_PREF_PRIMARY_SUB,
+                            mPhone.getSubId());
+                    log("Updating user pref primary subId: " + mPhone.getSubId() + ", in DB");
+                }
+            }
+            return;
         }
 
         private void handleGetPreferredNetworkTypeResponse(Message msg) {
@@ -763,6 +782,7 @@ public class MSimMobileNetworkSubSettings extends PreferenceActivity
                 setPreferredNetworkMode(networkMode);
                 updateButtonPreferredLte();
                 updateButtonEnable4g();
+                updateUserPrefPrimarySubIdInDB();
                 if (SystemProperties.getBoolean(PROPERTY_GTA_OPEN_KEY, false))
                     setPrefNetworkTypeInDb(networkMode);
             } else {
