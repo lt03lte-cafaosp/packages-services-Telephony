44f0d8cfcfabd475cc9f0392a75a0d9063023a05
Fang Yunong <yunong@codeaurora.org>
2016-05-09 17:17:26 +0800

    Telephony: modify PreferenceScreen
    modify xml PreferenceScreen removeScreen item
    
    Change-Id: I56ecec11e09b25b65d206538ab301f83d6732dac
    CRs-Fixed: 1011009

--- src/com/android/phone/ims/WifiCallingSettings.java
+++ src/com/android/phone/ims/WifiCallingSettings.java
@@ -158,7 +158,7 @@ public class WifiCallingSettings extends PreferenceActivity
                 screen.removePreference(mWifiCallingHelp);
             }
             if (mWifiCallingConnectPre != null) {
-                screen.removePreference(screen);
+                screen.removePreference(mWifiCallingConnectPre);
             }
             return;
         } else {
