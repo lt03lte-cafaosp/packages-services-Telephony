0cd321871de60a61c207410761e05b9e93d86769
Jack Yu <jackyu@google.com>
2017-01-16 10:15:34 -0800

    Added permission check for setCellInfoListRate
    Now the caller needs to have MODIFY_PHONE_STATE to call
    this API.
    
    Test: Manual
    bug: 33815946
    Change-Id: Ia2cf0de383572033c70b94ae71080ef35c3f443c
    (cherry picked from commit 3128d9ec40fadccbf2be7b9e962e35b9bfbc003a)

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -1637,6 +1637,7 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
 
     @Override
     public void setCellInfoListRate(int rateInMillis) {
+        enforceModifyPermission();
         mPhone.setCellInfoListRate(rateInMillis);
     }
 
